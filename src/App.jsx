1a03d910a115c64610975f2279b45178eb0ba7bf
Evan Dooner <evanjdooner@gmail.com>
2024-07-23 22:21:19 +0100

    Use Unit object to drive unit card UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,25 @@
 // import { useState } from 'react'
 import './App.css'
+import Armour from './models/armour';
+import Class from './models/class';
+import Unit from './models/unit';
+
+let klass = new Class(
+  'Heavy Battle Vehicle',
+  10,
+  22,
+  'Heavy',
+  3,
+  8,
+  6,
+  3,
+  new Armour(2, 1, 1),
+  8,
+  ['Hull', 'Sponsons', 'Turret'],
+  ['Relentless']
+);
+
+let unit = new Unit(klass);
 
 function App() {
   // const [count, setCount] = useState(0)
@@ -7,25 +27,25 @@ function App() {
   return (
     <>
       <div className="unit">
-        <div className="name">Arcus Heavy Tank</div>
+        <div className="name">{unit.name}</div>
         <div className="cost title">Cost</div>
-        <div className="cost value">20</div>
+        <div className="cost value">{unit.cost}</div>
         <div className="size title">Size</div>
-        <div className="size value">Heavy</div>
+        <div className="size value">{unit.size}</div>
         <div className="discipline title">Discipline</div>
-        <div className="discipline value">3</div>
+        <div className="discipline value">{unit.discipline}</div>
         <div className="optics title">Optics</div>
-        <div className="optics value">8</div>
+        <div className="optics value">{unit.optics}</div>
         <div className="movement title">Movement</div>
-        <div className="movement value">6</div>
+        <div className="movement value">{unit.movement}</div>
         <div className="morale title">Morale</div>
-        <div className="morale value">4</div>
+        <div className="morale value">{unit.morale}</div>
         <div className="armour title">Armour</div>
-        <div className="armour value">2/2/1</div>
+        <div className="armour value">{unit.armour.front}/{unit.armour.sides}/{unit.armour.rear}</div>
         <div className="hullpoints title">Hull Points</div>
-        <div className="hullpoints value">8</div>
+        <div className="hullpoints value">{unit.hullPoints}</div>
         <div className="special title">Special</div>
-        <div className="special value">Relentless</div>
+        <div className="special value">{unit.special}</div>
         <div className="weapons">
           <div className="weapon title">Weapons</div>
           <div className="rating title">Rating</div>
