678dd5e67872b8cbb707638c83d893968b3674e3
Evan Dooner <evanjdooner@gmail.com>
2024-07-27 08:19:22 +0100

    Allow Mount to contain a Weapon

--- src/models/mount.ts
+++ src/models/mount.ts
@@ -1,13 +1,34 @@
-import { WeaponType, WeaponTypes } from "./weapon-class";
-import { compatibleWeaponClasses, MountType } from "./mount-type";
+import { compatibleWeaponTypes, MountType } from "./mount-type";
+import Weapon from "./weapon";
 
 export class Mount {
     readonly type: MountType;
     readonly index: number;
+    private _weapon: Weapon | null;
 
     constructor(type: MountType, index: number) {
         this.type = type;
         this.index = index;
+        this._weapon = null;
+    }
+
+    get weapon() {
+        return this._weapon || null;
+    }
+
+    get key() {
+        return `${this.type.size}-${this.type.mountType}-${this.index}`;
+    }
+
+    setWeapon(weapon: Weapon) {
+        if (!compatibleWeaponTypes(this.type).includes(weapon.type)) {
+            throw new Error(`Incompatible weapon type: ${weapon.type.name}`);
+        }
+        this._weapon = weapon;
+    }
+
+    removeWeapon() {
+        this._weapon = null;
     }
 
     compatibleWeaponTypes() {
