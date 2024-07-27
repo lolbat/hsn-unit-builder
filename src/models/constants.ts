7079d3a1eb5e24ad1eeba2a92c1c3629b9b2c646
Evan Dooner <evanjdooner@gmail.com>
2024-07-27 16:06:14 +0100

    Map weapon name strings to WeaponNames

--- src/models/constants.ts
+++ src/models/constants.ts
@@ -45,6 +45,49 @@ export enum WeaponName {
     MagmaWrecker = 'Magma Wrecker',
 }
 
+export function toWeaponName(weaponName: string) {
+    switch (weaponName) {
+        // Light Weaponry
+        case 'Autogun': return WeaponName.Autogun;
+        case 'Autocannon': return WeaponName.Autocannon;
+        case 'Howler': return WeaponName.Howler;
+        case 'Laser': return WeaponName.Laser;
+        case 'Light Missile': return WeaponName.LightMissile;
+        case 'Plasma Blaster': return WeaponName.PlasmaBlaster;
+        case 'Rotor Cannon': return WeaponName.RotorCannon;
+        case 'Light Cannon': return WeaponName.LightCannon;
+        // Heavy Weaponry
+        case 'Pulse Laser': return WeaponName.PulseLaser;
+        case 'Blast Cannon': return WeaponName.BlastCannon;
+        case 'Battle Cannon': return WeaponName.BattleCannon;
+        case 'Heavy Rotor Cannon': return WeaponName.HeavyRotorCannon;
+        case 'Heavy Cannon': return WeaponName.HeavyCannon;
+        case 'Heavy Laser': return WeaponName.HeavyLaser;
+        case 'Heavy Missile': return WeaponName.HeavyMissile;
+        case 'Magma Blaster': return WeaponName.MagmaBlaster;
+        case 'Plasma Cannon': return WeaponName.PlasmaCannon;
+        case 'Screamer': return WeaponName.Screamer;
+        // Superheavy Weaponry
+        case 'Super Blast Cannon': return WeaponName.SuperBlastCannon;
+        case 'Super Battle Cannon': return WeaponName.SuperBattleCannon;
+        case 'Missile Rack': return WeaponName.MissileRack;
+        case 'Magma Cannon': return WeaponName.MagmaCannon;
+        case 'Super Plasma Cannon': return WeaponName.SuperPlasmaCannon;
+        case 'Superheavy Laser': return WeaponName.SuperheavyLaser;
+        // Bombs
+        case 'Cluster Bombs': return WeaponName.ClusterBombs;
+        case 'Incendiary Bombs': return WeaponName.IncendiaryBombs;
+        case 'Heavy Bombs': return WeaponName.HeavyBombs;
+        case 'Plasma Bombs': return WeaponName.PlasmaBombs;
+        // Close Combat Weaponry
+        case 'Blade': return WeaponName.Blade;
+        case 'Wrecker': return WeaponName.Wrecker;
+        case 'Turbo-blade': return WeaponName.TurboBlade;
+        case 'Magma Wrecker': return WeaponName.MagmaWrecker;
+        default: throw new Error(`Unrecognised weapon name: ${weaponName}`);
+    }
+}
+
 export enum WeaponCategory {
     Projectile,
     Bomb,
