002e94c144f9d799f24530f51a2af47600b7cba5
Evan Dooner <evanjdooner@gmail.com>
2024-07-28 12:41:22 +0100

    Support linting of TypeScript files

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,12 +6,14 @@ module.exports = {
     "plugin:react/recommended",
     "plugin:react/jsx-runtime",
     "plugin:react-hooks/recommended",
+    "plugin:@typescript-eslint/recommended",
     "prettier",
   ],
+  parser: "@typescript-eslint/parser",
   ignorePatterns: ["dist", ".eslintrc.cjs"],
   parserOptions: { ecmaVersion: "latest", sourceType: "module" },
   settings: { react: { version: "18.2" } },
-  plugins: ["react-refresh"],
+  plugins: ["react-refresh", "@typescript-eslint"],
   rules: {
     "react/jsx-no-target-blank": "off",
     "react-refresh/only-export-components": [
