026426dab4b2718d1dffa49a6ceb0e4674d4faa1
Evan Dooner <evanjdooner@gmail.com>
2024-08-04 20:32:37 +0100

    Update changelog and bump version

--- src/models/changelog.ts
+++ src/models/changelog.ts
@@ -62,11 +62,19 @@ export const Changelog: ChangelogEntry[] = [
     description:
       "Added Upgrades and Compromises. Only costs have been implemented. Variable costs, multiple instaces of a modification, and changes to the unit stat card are not yet supported.",
   },
+  {
+    index: 6,
+    date: "2024-08-04",
+    version: new Version(0, 0, 7),
+    description:
+      "Added the ability to apply most Modifications. Modifications that have not yet been implemented are shown in red.",
+  },
 ];
 
 export const Todos: string[] = [
   "90% of the UI formatting and layout",
   "Add support for Mega-turret sponsons",
+  "Implement remaining modifications",
   "Rules text for special rules and upgrades/modifications",
   "List units saved to storage",
   "Delete a stored unit",
