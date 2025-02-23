062e21a64cff75caae167471f9707081dc28fec3
Evan Dooner <evanjdooner@gmail.com>
2025-02-23 18:44:16 +0000

    Try to set URL basename dynamically

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,18 @@ import App from "./App.tsx";
 import "./index.css";
 import { BrowserRouter, Route, Routes } from "react-router";
 
+const pathname = window.location.pathname;
+
+let basename;
+if (pathname.includes('hsn-unit-builder')) {
+  basename = '/hsn-unit-builder';
+} else {
+  basename = '';
+}
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={basename}>
       <Routes>
         <Route path="/" element={<App />} />
       </Routes>
